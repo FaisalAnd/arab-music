e8dc992028850acf8f0294ea2477b55a2d111a93
FaisalAnd <45271327+FaisalAnd@users.noreply.github.com>
2018-11-30 10:11:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("احسن بوت اغاني في العالم",{type: 'WATCHING'})
+   client.user.setActivity("BEST BOT : -help",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
